af591bd570d5e6d3d0c540b87516b2ec33925989
Arun Kumar <stockerunknown@gmail.com>
2025-01-26 00:22:02 +0530

    Created UI/UX of Feed Page Edit Page Profile Page Header Page Also Add Functionalities for Authentication And Authorization And Setup Multer

--- routes/index.js
+++ routes/index.js
@@ -1,30 +1,95 @@
 var express = require('express');
 var router = express.Router();
+const UserModel = require('./users');
+const passport = require('passport');
+const localStrategy = require('passport-local');
+const upload = require('./multer');
 
+passport.use(new localStrategy(UserModel.authenticate()));
 /* GET home page. */
 
+
+// Home
+router.get('/', function (req, res, next) {
+  res.render('index',);
+});
 // LOGIN
 router.get('/login', function (req, res, next) {
   res.render('login', { title: 'Express' });
 });
 
+// Protected Routes Starting
+// Profile
+router.get('/profile', isLoggedIn, function (req, res, next) {
+  res.render('profile', { title: 'Express' });
+});
+// Feed
+router.get('/feed', isLoggedIn, function (req, res) {
+  res.render('feed')
+});
+// Update
+router.get('/edit', function (req, res) {
+  res.render('edit')
+});
+//  Protected Routes Ending
 
-router.get('/', function (req, res, next) {
-  res.render('index',);
+
+// POST METHOD ROUTES
+
+router.post('/register', function (req, res) {
+  const user = new UserModel({
+    name: req.body.name,
+    username: req.body.username,
+    email: req.body.email,
+  });
+  UserModel.register(user, req.body.password)
+    .then(function (registeredUser) {
+      passport.authenticate('local')(req, res, function () {
+        res.redirect('/profile')
+      })
+    })
 });
 
 
+// Login
+router.post('/login', passport.authenticate('local', {
+  successRedirect: '/profile',
+  failureRedirect: '/login'
+}), function (req, res) {
+
+  res.send('login')
+});
 
 
-router.post('/signup', function (req, res, next) {
-
-
-
+// Logout
+router.get('/logout', function (req, res) {
+  req.logout(function (err) {
+    if (err) { return next(err) };
+  })
+  res.redirect('/')
 
 })
 
 
+// Upload
+router.post('/upload', upload.single('file'), async function (req, res) {
+  
+  if (!req.file) {
+    return res.status(400).send('Please upload a file')
+  }
+  const user=await UserModel.findOneAndUpdate({username:req.session.passport.user});
+  res.send('File uploaded successfully')
 
+});
+
+
+// Custom Middleware
+function isLoggedIn(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  res.redirect('/login')
+}
 
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,39 @@
-var express = require('express');
-var router = express.Router();
+const { name } = require('ejs');
+const mongoose = require('mongoose');
+const plm = require('passport-local-mongoose');
+mongoose.connect('mongodb://127.0.0.1:27017/Instagram');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
 
-module.exports = router;
+const userSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    maxlength: 20,
+  },
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    maxlength: 20,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+  },
+  password: {
+    type: String,
+    trim: true,
+  },
+  profileImage: {
+    type: String
+  },
+  posts: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Post'
+  }]
+})
+userSchema.plugin(plm);
+module.exports = new mongoose.model('User', userSchema);
